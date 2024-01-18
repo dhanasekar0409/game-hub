133d0f2c3646320842f4b1729bc79738fc289417
Dhanasekar <dhanasekarn0409@gmail.com>
2024-01-18 13:15:19 +0530

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html")
+        // main: path.resolve(__dirname, 'main.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "./node_modules/bootstrap")
+    }
+  },
+  plugins: [react()],
+  server: {
+    // open: './main.html',
+    open: "./"
+  }
+});
