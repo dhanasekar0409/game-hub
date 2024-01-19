88489c5699456b53fb3612d0b04599ed3f9326eb
Dhanasekar <dhanasekarn0409@gmail.com>
2024-01-19 19:20:01 +0530

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,23 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
-import userService, { User } from "./services/user-service";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-const App = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAllUsers();
-    request
-      .then((res) => {
-        setUsers(res.data);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
-
-  const deleteUser = (user: User) => {
-    const originalUsers = [...users];
-    setUsers(users.filter((u) => u.id !== user.id));
-
-    userService.deleteUser(user.id).catch((err) => {
-      setError(err.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  const addUser = () => {
-    const originalUsers = [...users];
-    const newUser = { id: 0, name: "Dhana" };
-    setUsers([newUser, ...users]);
-
-    userService
-      .creatUser(newUser)
-      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
-      .catch((err) => {
-        setError(err.message);
-        setUsers(originalUsers);
-      });
-  };
-
-  const updateUser = (user: User) => {
-    const originalUsers = [...users];
-    const updatedUser = { ...user, name: user.name + "!" };
-    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
-
-    userService.updateUser(updatedUser).catch((errr) => {
-      setError(errr.message);
-      setUsers(originalUsers);
-    });
-  };
-
-  return (
-    <>
-      {error && <p className="text-danger fw-bold">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>
-        Add
-      </button>
-      <ul className="list-group">
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className="list-group-item d-flex justify-content-between">
-            {user.name}
-            <div>
-              <button
-                className="btn btn-secondary mx-2"
-                onClick={() => updateUser(user)}>
-                Update
-              </button>
-              <button
-                className="btn btn-danger"
-                onClick={() => deleteUser(user)}>
-                Delete
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+const App = () => (
+  <Grid
+    templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }}>
+    <GridItem area="nav" bg="coral">
+      Nav
+    </GridItem>
+    <Show above="lg">
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+    </Show>
+    <GridItem area="main" bg="dodgerblue">
+      Main
+    </GridItem>
+  </Grid>
+);
 
 export default App;
--- src/Main.tsx
+++ src/Main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/styles.scss";
 import App from "./App";
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
