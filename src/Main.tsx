b3e854d635f2319e8d919addcda9ccd31ec6ef69
Dhanasekar <dhanasekarn0409@gmail.com>
2024-01-19 19:38:17 +0530

    Build a NavBar

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/styles.scss";
 import App from "./App";
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider } from "@chakra-ui/react";
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root as HTMLElement).render(
