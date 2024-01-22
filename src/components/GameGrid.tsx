36ed38831a6330e275773bd42df73176618860f9
Dhanasekar <dhanasekarn0409@gmail.com>
2024-01-22 22:01:19 +0530

    display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text} from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -12,8 +12,8 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={6}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={3}
         padding="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
