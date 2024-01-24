a644fc2ab9f0f6a64c691dcd0562ab8b70b16e5a
Dhanasekar <dhanasekarn0409@gmail.com>
2024-01-24 21:13:09 +0530

    Clean up the genres

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
-import { Genre } from "./hooks/useGenres";
-import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
+import PlatformSelector from "./components/PlatformSelector";
+import { Genre } from "./hooks/useGenres";
+import { Platform } from "./hooks/useGames";
 
 export interface GameQuery {
   genre: Genre | null;
