06d2264c5484538f4dd092e6db633e0ddd25f842
Dhanasekar <dhanasekarn0409@gmail.com>
2024-02-11 15:51:41 +0530

    Implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -32,6 +32,7 @@ const useGames = (gameQuery: GameQuery) =>
         },
       }),
     initialPageParam: 1,
+    staleTime: 24 * 60 * 60 * 1000, //24h
     getNextPageParam: (lastpage, allPages) => {
       return lastpage.next ? allPages.length + 1 : undefined;
     },
